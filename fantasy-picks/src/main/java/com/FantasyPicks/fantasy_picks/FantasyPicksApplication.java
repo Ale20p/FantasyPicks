67024a8137f1649fd305f9b10cbe7627f0701b52
Alessandro Pomponi <alescode1@gmail.com>
2026-04-25 17:08:33 -0400

    addition of the Java project to the repo

---
+++ fantasy-picks/src/main/java/com/FantasyPicks/fantasy_picks/FantasyPicksApplication.java
@@ -0,0 +1,13 @@
+package com.FantasyPicks.fantasy_picks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FantasyPicksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FantasyPicksApplication.class, args);
+	}
+
+}
---
+++ fantasy-picks/src/test/java/com/FantasyPicks/fantasy_picks/FantasyPicksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FantasyPicks.fantasy_picks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FantasyPicksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
